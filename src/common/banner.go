0245bc9499c21c54d405a3e5a9f43a7dc5756f88
Max Amelchenko <maxam@post.bgu.ac.il>
2022-09-19 13:58:00 +0300

    Update banner.go
    add test secrets to repo

--- src/common/banner.go
+++ src/common/banner.go
@@ -1,3 +1,10 @@
 package common
 
 const YorLogo = "  __    __\n  \\ \\  / /\n   \\ \\/ /___  _  ____\n    \\  /  _ \\| |/  __|\n    | |  |_| |   /\n    |_|\\____/|__|"
+
+const SEC_1 = "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"
+const SEC_2 = "eyJrIjoiNUwyZU7TMmRxQXNVcnR7UXB0ME4zYkhRaTk2STVhR0MiLCJuIjoidGVtcCIsImlkIjoxfQ=="
+const SEC_3 = "dsapi45202d12abdce73c004a9e0be24a21b2"
+
+const CIRCLE_CI = "2065ae463be5e534bb1d074a366d44e7a776d472"
+const JIRA = "5FP0NmFYz81U32XdjNb42762"
